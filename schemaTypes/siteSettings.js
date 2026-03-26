c650e61cdd29ab702795218073404fac90172a67
Henrique <henrique.malafaia@gmail.com>
2026-03-26 20:07:05 -0300

    feat: Add bodyFont field to site settings for customizable body text font.

--- schemaTypes/siteSettings.js
+++ schemaTypes/siteSettings.js
@@ -12,5 +12,12 @@ export const siteSettings = defineType({
       description:
         'Digite o nome exato da fonte conforme o Google Fonts (ex: "Playfair Display", "Lora", "Merriweather"). A fonte será aplicada automaticamente nos títulos do site.',
     }),
+    defineField({
+      name: 'bodyFont',
+      title: 'Fonte do Corpo do Texto',
+      type: 'string',
+      description:
+        'Digite o nome exato da fonte conforme o Google Fonts (ex: "Lato", "Open Sans", "Roboto"). A fonte será aplicada nos parágrafos e textos do corpo do site.',
+    }),
   ],
 })
